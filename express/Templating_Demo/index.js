becfdf140033a7e4ad917d3e3fafef2e6d361003
seiberttregoning <georgetregoning@gmail.com>
2022-02-18 11:58:02 -0600

    adding ejs views code to make views lookup dependent on index file instead of run directory

--- express/Templating_Demo/index.js
+++ express/Templating_Demo/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path'); // Needed to update ejs views directory lookup
 
 app.set('view engine', 'ejs');
+
+// This sets up ejs to find the /views directory in relation
+// to the index file rather than the directory the code was run from
+app.set('views', path.join(__dirname, '/views'))
 
 app.get('/', (req, res) => {
     res.render('home');
