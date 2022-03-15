a83b766648ba68b672b5917d6cf2dabc42ee304f
seiberttregoning <georgetregoning@gmail.com>
2022-03-15 09:23:43 -0500

    small edits to templating files

--- express/Templating_Demo/index.js
+++ express/Templating_Demo/index.js
@@ -6,13 +6,26 @@ app.set('view engine', 'ejs');
 
 // This sets up ejs to find the /views directory in relation
 // to the index file rather than the directory the code was run from
-app.set('views', path.join(__dirname, '/views'))
+app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.get('/random', (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1;
+    // res.render('random', { rand: num });
+    // Can also pass like this:
+    res.render('random', { num });
+});
+
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    res.render('subreddit', { subreddit })
 })
 
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
-})
+});
